2bf6ac1e76e58440bb13d24a94af278e20217e61
Phil Chodrow <pchodrow@mit.edu>
2019-06-04 11:06:50 -0400

    update core scripts

--- C/run.py
+++ C/run.py
@@ -7,21 +7,21 @@ count=len(os.listdir('data'));
 node=10000; # number of nodes
 c = np.array([4, 6, 8, 20]); # mean degree 
 
-dt=5000; # save results every dt steps
-alph = np.linspace(1.0, 0.0, 101) # rewiring probability
-lam = 2.0**(-np.array([2.0, 4.0, 6.0, 8.0, 10.0]))
+dt=10000; # save results every dt steps
+alph = np.linspace(1.0, 0.0, 21) # rewiring probability
+lam = 2.0**(-np.array([8.0, 12.0, 16.0]))
 l_01 = np.arange(0, 1, 11)
 g=2; # number of opinions
 
-gam = np.linspace(0.1, .9, 9)
-max_steps = 10**6
-realizations = np.arange(0,10)
+gam = np.array([.5])
+max_steps = 10**7
+realizations = np.arange(0,5)
 
 # U_naught = np.linspace(0, 1.0, 11)
-U_naught = [.5]
+U_naught = [.1, .25, .5, .75, .9]
 
-U0=1.0/g*np.ones(g,); # list of initial densities
-U0[-1]=1-sum(U0[:-1]);
+# U0=1.0/g*np.ones(g,); # list of initial densities
+# U0[-1]=1-sum(U0[:-1]);
 mode = [0, 1]
 
 pars = product(realizations, lam, gam, c, mode, alph, U_naught)
@@ -35,7 +35,7 @@ for realization, lamb, gamma, cee, mode, alpha, U0 in pars:
 #        cmd=cmd
     print cmd
     subprocess.call(cmd, shell=True);
-    count=count+1;
+    count+=1;
 
 print str(count-1)+' files submitted.';
 print 'Mission Complete!';
